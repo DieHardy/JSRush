7eca60b4523d2e2565e37bb2791bb84c2103d1ed
Nikita <nerdyaus21@gmail.com>
2023-12-15 21:04:34 +0300

    upd: 2 part

--- udemy/task1/js/index.js
+++ udemy/task1/js/index.js
@@ -14,15 +14,26 @@ let personalMovieDB = {
 
 const moviesCount = () => {
   personalMovieDB.count = +prompt("How many movies did you watch?");
+  if( personalMovieDB.count < 11) alert(`You watched: ${personalMovieDB.count} movies! That's such a small amount of movies!`);
+  if( personalMovieDB.count < 31 &&  personalMovieDB.count > 10) alert(`You watched: ${personalMovieDB.count} movies! You're a classical viewer!`);
+  if( personalMovieDB.count > 30) alert(`You watched: ${personalMovieDB.count} movies! You love movies :)`);
+
+
   console.log("You watched: " + personalMovieDB.count + " movies!");
 };
 const checkLast = () => {
 for(let i = 0; i < 2; i++){
-  personalMovieDB.movies['name'][i] = prompt("What was your last movie?");
-  console.log("Your last movie was: " + personalMovieDB.movies.name[i]);
-  personalMovieDB.movies['rate'][i] = +prompt("Give the movie a grade from 1 to 10");
-  console.log("Your grade is: " + personalMovieDB.movies.rate[i]);
-}
+    let a = prompt("What was your last movie?");
+    if(a.length < 50 && a.length > 0) {
+    let b = +prompt("Give the movie a grade from 1 to 10");
+    if(b > 0 && b < 11) {
+      personalMovieDB.movies['rate'][i] = b; 
+      personalMovieDB.movies['name'][i] = a;
+    }
+    else {alert('The movie`s grade must be between 1 and 10.'); break;}
+  }
+    else {alert('The movie`s name must be between 1 and 50 characters.'); break;}
+  }
 };
 
 movies.addEventListener("click", moviesCount);
